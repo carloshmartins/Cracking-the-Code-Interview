6e25625bc491a757c145381d675b790621f0dfe4
Carlos Henrique <martinscarloshenriquej@gmail.com>
2018-02-25 20:09:42 -0300

    Created a algorithm to solve
    Still not working.

---
+++ Staircase Algorithm.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let arraySize = 10
+var emptyArray = String()
+var hashArray = String()
+
+for _ in 0...10{
+    emptyArray.append(" ")
+}
+
+for _ in 0...10{
+    emptyArray.append("#")
+    print(emptyArray)
+}

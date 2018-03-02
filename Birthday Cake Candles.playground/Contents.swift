13c305d869e3c36184a3e9d65b3b759530df8f34
Carlos Henrique <martinscarloshenriquej@gmail.com>
2018-03-02 10:54:33 -0300

    Solution #3

--- Birthday Cake Candles.playground/Contents.swift
+++ Birthday Cake Candles.playground/Contents.swift
@@ -1,16 +1,18 @@
 import Foundation
 
-let arrayCandles = [6,4,5,2,1,2]
-let n = arrayCandles.count
+// Enter your code here 
+let n = Int(readLine()!)!
+let arrayCandles = readLine()!.components(separatedBy:" ").map{Int($0)!}
 var highestCandle = 0
 var highestCandlerCounter = Int()
+
 //Check which numbers is the highest number
-for i in 0...n-1 {
+for i in 0...n {
     if arrayCandles[i] > highestCandle {
         highestCandle = arrayCandles[i]
     }
 }
-for i in 0...n-1 {
+for i in 0...n {
     if arrayCandles[i] == highestCandle {
         highestCandlerCounter += 1
     }

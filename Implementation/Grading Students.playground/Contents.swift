edd8ab170230e2a1183514afd8d32d20fbd1185c
Carlos Henrique <martinscarloshenriquej@gmail.com>
2018-03-21 09:11:14 -0300

    Transferred the Algorithm question into Xcode Playground

--- Implementation/Grading Students.playground/Contents.swift
+++ Implementation/Grading Students.playground/Contents.swift
@@ -1,28 +1,32 @@
-/*
-HackerLand University has the following grading policy:
-
-Every student receives a  in the inclusive range from  to .
-Any  less than  is a failing grade.
-Sam is a professor at the university and likes to round each student's  according to these rules:
-
-If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
-If the value of  is less than , no rounding occurs as the result will still be a failing grade.
-For example,  will be rounded to  but  will not be rounded because the rounding would result in a number that is less than .
-
-Given the initial value of  for each of Sam's  students, write code to automate the rounding process. Complete the function solve that takes an integer array of all grades, and return an integer array consisting of the rounded grades. For each , round it according to the rules above and print the result on a new line.
-
-Input Format
-
-The first line contains a single integer denoting  (the number of students).
-Each line  of the  subsequent lines contains a single integer, , denoting student 's grade.
-
-Constraints
-
-Output Format
-
-For each  of the  grades, print the rounded grade on a new line.
-
-*/
-
 import Foundation
+
+/*
+ * Complete the gradingStudents function below.
+ */
+func gradingStudents(grades: [Int]) -> [Int] {
+    /*
+     * Write your code here.
+     */
+    
+}
+
+let fileName = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: fileName, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: fileName)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+let grades: [Int] = AnyIterator{ readLine() }.prefix(n).map {
+    if let gradesItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return gradesItem
+    } else { fatalError("Bad input") }
+}
+
+guard grades.count == n else { fatalError("Bad input") }
+
+let result = gradingStudents(grades: grades)
+
+fileHandle.write(result.map{ String($0) }.joined(separator: "\n").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
 
